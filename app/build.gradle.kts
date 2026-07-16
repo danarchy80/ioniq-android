5e9221f03365021196f62d324cb2acb607575968
Hermes <hermes@ioniq.dev>
2026-07-16 08:23:00 -0400

    Bump version to 1.0.0-beta.3 (settings cog + crash fixes)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.ioniq.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0-beta.2"
+        versionCode = 3
+        versionName = "1.0.0-beta.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
